7903436ff7ed05f75e257e9dead8b1e6d82c83c2
heyaoyu <heyaoyu@gmail.com>
2017-10-10 14:21:03 +0800

    unknown

--- common-main/src/main/java/org/heyaoyu/tutorials/BigIntToChar.java
+++ common-main/src/main/java/org/heyaoyu/tutorials/BigIntToChar.java
@@ -6,10 +6,21 @@ package org.heyaoyu.tutorials;
 public class BigIntToChar {
 
   public static void main(String[] args) {
-    int i = 10000;
-    char c = (char) i;
-    System.out.println(i);
-    System.out.println(c);
+//    int i = 10000;
+//    char c = (char) i;
+//    System.out.println(i);
+//    System.out.println(c);
+    String h5Url = "http://m.toutiao.iqiyi.com/top_11uthn42bi.html?inBase=1&entry=daily_info4";
+    System.out.println(extractNewsIdFromH5Url(h5Url));
+  }
+
+  private static String extractNewsIdFromH5Url(String h5Url) {
+    int top_ = h5Url.indexOf("top_");
+    int dotHtml = h5Url.lastIndexOf(".html");
+    if (dotHtml > top_ + 4) {
+      return h5Url.substring(top_ + 4, dotHtml);
+    }
+    return "";
   }
 
 }
--- couchbase-usage/src/main/java/org/heyaoyu/tutorials/App.java
+++ couchbase-usage/src/main/java/org/heyaoyu/tutorials/App.java
@@ -1,13 +1,32 @@
 package org.heyaoyu.tutorials;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.couchbase.client.CouchbaseClient;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import net.spy.memcached.internal.OperationFuture;
+
+public class App {
+
+  public static void main(String[] args)
+      throws IOException, ExecutionException, InterruptedException {
+    URI uri = URI.create("http://10.153.72.243:8091");
+    List<URI> uriList = new ArrayList<URI>() {{
+      this.add(uri);
+    }};
+    CouchbaseClient client = new CouchbaseClient(uriList, "mbd-toutiao", "");
+    System.out.println(new String((byte[]) client.get("vote:deviceId:aaa:newsId:406808200570")));
+    OperationFuture<Map<String, String>> future = client
+        .getKeyStats("vote:deviceId:aaa:newsId:406808200570");
+    Map<String, String> map = future.get();
+    for (Map.Entry<String, String> entry : map.entrySet()) {
+      System.out.println("Key:" + entry.getKey());
+      System.out.println("Value:" + entry.getValue());
     }
+    client.shutdown();
+  }
+
 }
